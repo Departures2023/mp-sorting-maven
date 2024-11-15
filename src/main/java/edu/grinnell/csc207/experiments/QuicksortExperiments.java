eca181bb3eb0f56f77c26206c01dee2e6dffa39d
KevinTang <tangkevi@grinnell.edu>
2024-11-14 20:08:54 -0600

    Finalize

--- src/main/java/edu/grinnell/csc207/experiments/QuicksortExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/QuicksortExperiments.java
@@ -1,6 +1,6 @@
 package edu.grinnell.csc207.experiments;
 
-import edu.grinnell.csc207.sorting.Quicksorter;
+import edu.grinnell.csc207.sorting.QuickSorter;
 import edu.grinnell.csc207.sorting.Sorter;
 
 import java.io.PrintWriter;
@@ -26,7 +26,7 @@ public class QuicksortExperiments {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     Comparator<Comparable> order = (x, y) -> x.compareTo(y);
-    Sorter<Comparable> sorter = new Quicksorter(order);
+    Sorter<Comparable> sorter = new QuickSorter(order);
     SortExperiments.manyExperiments(pen, sorter);
     SortExperiments.oneExperiment(pen, args, sorter);
     pen.close();
